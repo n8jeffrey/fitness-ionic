45c31d51fcfa369b674734ac01c3af5ac6b531e2
Nathan <n8jeffrey@gmail.com>
2021-03-23 00:35:37 -0300

    Added firebase config files structure and content

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBgoZ74u86G74GuPaOZItQ7q-E2OhOuGnQ",
+    authDomain: "health-83d57.firebaseapp.com",
+    projectId: "health-83d57",
+    storageBucket: "health-83d57.appspot.com",
+    messagingSenderId: "476694149962",
+    appId: "1:476694149962:web:e8b30d227ace8dd7aa9fb3",
+    measurementId: "G-ZV8BW9SYMD"
+  };
+
+  export default firebaseConfig
