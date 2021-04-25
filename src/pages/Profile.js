82e08acb6742b295d1b4718263d528ad6728e5f3
Nathan <n8jeffrey@gmail.com>
2021-04-25 04:20:24 -0300

    Getting started on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonCard, IonContent, IonPage } from '@ionic/react';
+import { personCircleOutline, mailOutline } from 'ionicons/icons'
+import UserContext from '../contexts/UserContext'
+import firebase from '../firebase'
+import { toast } from '../utils/toast'
 import SmallHeader from '../components/header/SmallHeader'
 import LargeHeader from '../components/header/LargeHeader'
 
 const Profile = () => {
+    const {user} = React.useContext(UserContext)
+
+    async function logoutUser() {
+        try {
+            await firebase.logout();
+            props.history.push("/")
+            toast('You have successfully logged out.')
+        } catch (err) {
+            console.error('Logout Error', err)
+            toast(err.message)
+        }
+    }
+
     return(
     <IonPage>
         <SmallHeader title='Profile'/>
         <IonContent fullscreen>
-            <LargeHeader title='Profile'/>
+            <LargeHeader title='Profile'/>\
+            {user ? (
+                <>
+                    <IonCard>
+                </>
+            )}
         </IonContent>
     </IonPage>
     );
